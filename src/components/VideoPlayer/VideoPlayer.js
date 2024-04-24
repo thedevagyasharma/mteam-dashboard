e3d757ac54482470f40619cd8157ffb5c9dd04e1
Devagya Sharma <devagyas900@gmail.com>
2024-04-24 15:31:15 +0000

    VideoPlayer Test

---
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+const VideoPlayer = () => {
+    
+    return (
+        <ReactPlayer
+          url="<https://www.youtube.com/watch?v=_FHrraNmSNI>"
+          width="640"
+          height="360"
+        />
+      );
+
+}
+
+export default VideoPlayer;
